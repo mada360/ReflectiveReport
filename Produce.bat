b6aeb81ab35a594a03be9b434419eed3b5b2b5d3
Adam Worley <worley9@hotmail.co.uk>
2016-04-13 10:06:57 +0100

    Initial commit

---
+++ Produce.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+rem Populate the array with existent files in folder
+set i=0
+for %%a in (*.md) do (
+   set /A i+=1
+   set list[!i!]=%%a
+   set output[!i!]=%%~na
+)
+set Filesx=%i%
+
+
+rem Display array elements
+for /L %%i in (1,1,%Filesx%) do pandoc -t latex -f markdown !list[%%i]! -o !output[%%i]!.tex
+
+pdflatex.exe Report.tex
+
+Del /Q *.aux *.log
+
+Report.pdf
